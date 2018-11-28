88b49e3b731ac5b80d8c7ecc380a45f3e9ca60db
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-11-28 18:05:07 +0100

    feature: get rid of CLI proxy for WS connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {SocketIoModule} from 'ngx-socket-io';
-import {LoggerService} from './logger.service';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SocketIoModule } from 'ngx-socket-io';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoggerService } from './logger.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SocketIoModule.forRoot({url: 'http://localhost:4200/api'})
+    SocketIoModule.forRoot({
+      url: 'http://localhost:3000',
+    }),
   ],
   providers: [LoggerService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
